6ac75429847065cab15e33e957be1edab58ac56f
favygold <simepiriyefavour@gmail.com>
2022-07-17 11:16:06 +0000

    sorting

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+void insertion_sort_list(listint_t**list);
+{
+    for (int i = 1; i < size; i++)
+    {
+        int value = A[i];
+        int space = i;
+        while (space > 0 && A[space - 1] > value)
+        {
+            A[space] = A[space - 1];
+            space--;
+        }
+        A[space] = value;
+    }
+}
+
+int main()
+{
+    int A[] = {-3, 2, -1, -5, 6, 4, 0, 21, 100, -55, 310, 1000};
+
+    printf("\nInput array is:");
+    for (int i = 0; i < 12; i++)
+        printf(" %d", A[i]);
+
+    insertionSort(A, 12);
+
+    printf("\nSorted array is:");
+    for (int i = 0; i < 12; i++)
+        printf(" %d", A[i]);
+    printf("\n");
+
+    return 0;
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+void selection_sort(int*array, size_t size);
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		int min = i;
+		for (int j = i + 1; j < size; j++)
+		{
+			if (A[j] < A[min])
+			{
+				min = j;
+			}
+		}
+		int temp = A[min];
+		A[min] = A[i];
+		A[i] = temp;
+	}
+}
+
+int main()
+{
+	int A[] = {-3, 2, -1, -5, 6, 4, 0, 21, 100, -55, 310, 1000};
+
+	printf("\nInput array is:");
+	for (int i = 0; i < 12; i++)
+		printf(" %d", A[i]);
+
+	selectionSort(A, 12);
+
+	printf("\nSorted array is:");
+	for (int i = 0; i < 12; i++)
+		printf(" %d", A[i]);
+	printf("\n");
+
+	return 0;
+}
